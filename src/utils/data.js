fafdcf131c8390f42039edfd232b6dab66e8e836
Rana1017 <toprana212@gmail.com>
2023-05-06 22:30:04 +0545

    Added necessary data and created required folders

---
+++ src/utils/data.js
@@ -0,0 +1,112 @@
+export const data = {
+  /**Hero Page Related Data */
+  heroTitle: "Dream, Work and Succeed",
+  heroMoto: "Want to become a Soldier",
+  heroImage:
+    "https://drive.google.com/file/d/1w3T8xTAgY04JZE5WYSOUgfjItYIdNQVL/view?usp=share_link",
+  //About Section//
+  aboutImageUrl:
+    "https://drive.google.com/file/d/1le_z5g_0OymB9VdAAlH0akwSN8MZBJgH/view?usp=share_link",
+  aboutSection1:
+    "<p>Brave Gorkha is a government authorized & one of the leading institution in Western region of Nepal.It mainly focus  on recruiting trainee for Pre British Gurkha Army and Singapore Police Force. We are helping potential recruits to acheive their dreams who willing to join Gurkha Army. Brave Gorkha is a government authorized & one of the leading institution in Western region of Nepal. It mainly focus  on recruiting trainee for Pre British Gurkha Army and Singapore Police Force. We are helping potential recruits to acheive their dreams who willing to join Gurkha Army.</p>",
+  aboutSection2:
+    "<p>Brave Gorkha is a government authorized & one of the leading institution in Western region of Nepal.It mainly focus  on recruiting trainee for Pre British Gurkha Army and Singapore Police Force. We are helping potential recruits to acheive their dreams who willing to join Gurkha Army. Brave Gorkha is a government authorized & one of the leading institution in Western region of Nepal. It mainly focus  on recruiting trainee for Pre British Gurkha Army and Singapore Police Force. We are helping potential recruits to acheive their dreams who willing to join Gurkha Army.</p>",
+
+  /**Our Services Section Related Data */
+  services: [
+    {
+      title: "British Gurkha Army",
+      history: "British army has history of, blah blah or done ",
+      imageUrl:
+        "https://drive.google.com/file/d/1w3T8xTAgY04JZE5WYSOUgfjItYIdNQVL/view?usp=share_link",
+      btnLink: "Enroll Now",
+    },
+    {
+      title: "Singapore Police",
+      history: "Singapore Police has history of, blah blah or done ",
+      imageUrl:
+        "https://drive.google.com/file/d/1o2IXksqfLq4Bca5FZ9OmJWFVf4IboytS/view?usp=share_link",
+      btnLink: "Enroll Now",
+    },
+    {
+      title: "Indian Gurkha Army",
+      history: "Indian army has history of, blah blah or done ",
+      imageUrl:
+        "https://drive.google.com/file/d/1HGirKBTLspkXt8dfDjMBg69LaOzWr5pI/view?usp=share_link",
+      btnLink: "Enroll Now",
+    },
+    {
+      title: "Nepali Army",
+      history: "Nepali Army History.... ",
+      imageUrl:
+        "https://drive.google.com/file/d/1NBQh4b9uXI_AsCOhKMurf4BsQrOJouxw/view?usp=share_link",
+      btnLink: "Enroll Now",
+    },
+    {
+      title: "Nepali Police",
+      history: "Nepali police history....",
+      imageUrl:
+        "https://drive.google.com/file/d/1aNFqK2sQqTWhnyr6Gnw6azfrrcO1toko/view?usp=share_link",
+      btnLink: "Enroll Now",
+    },
+  ],
+  /***Testimonials Section */
+  testimonials: [
+    {
+      imageLink:
+        "https://drive.google.com/file/d/1hcr2jxsGKrneRuwpTsnootEV7cVOpFgP/view?usp=share_link",
+      statement: "One of the best training center in town",
+      name: "Ram Bahadur Thapa",
+      position: "trainee",
+    },
+    {
+      imageLink:
+        "https://drive.google.com/file/d/1STg2kmRXxM8o1IeV6ILr2Ad6JGQ578Nh/view?usp=share_link",
+      statement: "One of the best training center in town",
+      name: "Kaji Basnet",
+      position: "2016 intake",
+    },
+    {
+      imageLink:
+        "https://drive.google.com/file/d/10PRqxjvG1NENokA2fOS8eGQ0pcyNfejT/view?usp=share_link",
+      statement: "One of the best training center in town",
+      name: "Junga Bahadur Rana",
+      position: "2020 intake",
+    },
+  ],
+  //Downloads SEction//
+  downloadsSection: {
+    requirementLink:
+      "https://drive.google.com/file/d/1MBbEn-VaiZvPxrKkXgIX9_gNlHWtqN1o/view?usp=share_link",
+    questionLink:
+      "https://drive.google.com/file/d/18GuvtxOONZ5snmx6uM_WUQJ2gY8-FySV/view?usp=share_link",
+  },
+
+  Gallery: [
+    {
+      imageId: 1,
+      imageTitle: "British",
+      url: "https://drive.google.com/file/d/1yvlGC9wZowLQGqprRe9lNk3_paVd2BIG/view?usp=share_link",
+    },
+    {
+      imageId: 2,
+      imageTitle: "British",
+      url: "https://drive.google.com/file/d/1UR7G9-MJCAn28a5ZtSBZKiUKdrLnce41/view?usp=share_link",
+    },
+    {
+      imageId: 3,
+      imageTitle: "British",
+      url: "https://drive.google.com/file/d/1da8V2pfzmaQJfjWLRr3tLtlJRsXQX5cN/view?usp=share_link",
+    },
+    {
+      imageId: 4,
+      imageTitle: "Nepali",
+      url: "https://drive.google.com/file/d/1iSIImii1Fc8OAKhnjADRBNu5YBTNWBIM/view?usp=share_link",
+    },
+  ],
+
+  //**Contact Section */
+  email: "bravegurkha@gmail.comm",
+  phone: "9934980945",
+  address: "Begnas Lake, Pokhara, Nepal",
+};
